a853036b17aa2225ac9a898bb751f053bd2e8fbb
bcwan <bcwan8@gmail.com>
2020-12-20 00:24:39 -0800

    Update urls in main app

--- compound_power/compound_power/urls.py
+++ compound_power/compound_power/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include 
 
 urlpatterns = [
-    path('', include('users.urls')),
+    path('', include('frontend.urls')),
+    path('', include('users.urls'))
 ]
