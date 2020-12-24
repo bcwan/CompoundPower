816976d057e33310fa34f970be7c724d77ee4343
bcwan <bcwan8@gmail.com>
2020-12-23 23:20:02 -0800

    Remove logger

--- compound_power/frontend/src/store/store.js
+++ compound_power/frontend/src/store/store.js
@@ -5,7 +5,6 @@ import logger from 'redux-logger';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => (
-  //createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk, logger)))
   createStore(rootReducer, preloadedState, composeWithDevTools(applyMiddleware(thunk)))
 );
 
--- compound_power/users/models.py
+++ compound_power/users/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class User(models.Model):
   name = models.CharField(max_length = 50)
