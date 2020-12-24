e086646b35acbb863545629503b48d7ff55a2233
bcwan <bcwan8@gmail.com>
2020-12-23 18:25:41 -0800

    Create message actions

--- compound_power/frontend/src/actions/messages_actions.js
+++ compound_power/frontend/src/actions/messages_actions.js
@@ -1,2 +1,14 @@
 export const GET_MESSAGES = 'GET_MESSAGES';
 export const CREATE_MESSAGE = 'CREATE_MESSAGE';
+
+// GET MESSAGES
+const getMessages = messages => ({
+  type: GET_MESSAGES,
+  messages
+});
+
+// CREATE MESSAGE
+const createMessage = message => ({
+  type: CREATE_MESSAGE,
+  message
+});
