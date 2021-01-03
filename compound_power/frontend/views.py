94afe3fba991c09c0a66f7b36ff45988041d2557
bcwan <bcwan8@gmail.com>
2021-01-03 15:17:46 -0800

    Add comments in views.py in frontend

--- compound_power/frontend/views.py
+++ compound_power/frontend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here. This is pointing to index.html in src/templates/frontend
 def index(request):
   return render(request, 'frontend/index.html')
