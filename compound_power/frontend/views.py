3bbc5848de4b93a77c8e02fbece695ed2cfa6d8d
bcwan <bcwan8@gmail.com>
2021-01-07 14:33:53 -0800

    Change comments

--- compound_power/frontend/views.py
+++ compound_power/frontend/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here. This is pointing to index.html in src/templates/frontend
+# Create your views here. Pointing to index.html in src/templates/frontend
 def index(request):
   return render(request, 'frontend/index.html')
